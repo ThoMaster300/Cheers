a9ccc5340a6c35a6b292af8ab7a12d4f76d13ee2
alexkgithub <a.kimmel@gmx.at>
2018-01-03 20:17:21 +0100

    Warnhinweis-Dialog Wenn jemand daneben oder Schließen drückt, dann schließt sich die app

--- app/src/main/java/at/technikum_wien/cheers/MainActivity.java
+++ app/src/main/java/at/technikum_wien/cheers/MainActivity.java
@@ -1,6 +1,13 @@
 package at.technikum_wien.cheers;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +28,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        WarningDialogFragment warningDialog= new WarningDialogFragment();
+        warningDialog.show(getFragmentManager(), "warning");
+
+
+
         //Remove title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         //Remove notification bar
---
+++ app/src/main/java/at/technikum_wien/cheers/WarningDialogFragment.java
@@ -0,0 +1,47 @@
+package at.technikum_wien.cheers;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Alexander on 03.01.2018.
+ */
+
+public class WarningDialogFragment extends DialogFragment{
+
+
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setMessage(R.string.warningDialogMessage)
+                .setTitle(R.string.warningDialogTitle)
+                .setPositiveButton(R.string.warningDialogPositiveButton, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // ToDo: Wenn man OK klickt, muss man überhaupt eine Aktion starten
+                    }
+                })
+                .setNegativeButton(R.string.warningDialogNegativeButton, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Die app schließt sich
+                        getActivity().finish();
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+
+    //Wenn die Partypeople außerhalb des Dialogs drücken, wird die App auch geschlossen
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        super.onCancel(dialog);
+        getActivity().finish();
+    }
+}
